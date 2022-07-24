8fd0db43fd1d208b2f83c28e09082969be02ce22
Aurélien <au2001sfg@gmail.com>
2022-07-24 16:50:36 +0200

    Create timeline

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+import * as styles from "/src/styles/components/header.module.scss"
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      {/* TODO */}
+    </header>
+  )
+}
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,19 +1,22 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
-import * as styles from "../styles/404.module.scss"
+import * as styles from "/src/styles/404.module.scss"
 
 export default function NotFoundPage() {
   return (
-    <main className={styles.page}>
+    <>
       <title>Not found</title>
-      <h1 className={styles.heading}>Page not found</h1>
-      <p className={styles.paragraph}>
-        Sorry, we couldn’t find what you were looking for.
-        <br />
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+
+      <div className={styles.error}>
+        <h1 className={styles.heading}>404 - Page not found!</h1>
+        <p className={styles.paragraph}>
+          Sorry, we couldn’t find what you were looking for.
+          <br />
+          <br />
+          <Link to="/">Go home</Link>.
+        </p>
+      </div>
+    </>
   )
 }
